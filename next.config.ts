a24898803f0fb46f7f021e6d1bf98564c3737bca
Hassam-Ata <hassamata86@gmail.com>
2025-04-07 02:00:23 +0500

    configure ESLint, image remote patterns, and TypeScript settings in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to succeed even if there are type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
