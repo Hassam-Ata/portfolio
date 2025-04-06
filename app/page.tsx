d3fbf03e8ab2f5f04f7f2ef7a676b35ed4203fe4
Hassam-Ata <hassamata86@gmail.com>
2025-04-07 02:44:24 +0500

    refactor Vortex component props

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import { About } from "./(sections)/about";
 export default function Home() {
   return (
     <Vortex
-      backgroundColor="black"
-      className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-    >
+    backgroundColor="black"
+    rangeY={800}
+    particleCount={500}
+    baseHue={120}
+    className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+  >
       <About />
     </Vortex>
   );
